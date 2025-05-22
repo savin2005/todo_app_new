7a88aed6da4b7edcc79d3c8c29b434178b666163
Savin <titumr24@gmail.comm>
2025-05-22 11:44:33 +0530

    Updated UI for home screen

--- lib/view/splashscreen/splashscreen.dart
+++ lib/view/splashscreen/splashscreen.dart
@@ -6,8 +6,12 @@ class SplashscreenView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [],
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [Image.asset("assets/chatlogo.png"), Text("Make A Chat")],
+        ),
       ),
     );
   }
