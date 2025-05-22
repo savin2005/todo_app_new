51ef354d0f8b9e9252ee647b78953671bbbfd501
Savin <titumr24@gmail.comm>
2025-05-22 11:31:49 +0530

    Updated UI for home screen

---
+++ lib/view/splashscreen/splashscreen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class SplashscreenView extends StatelessWidget {
+  const SplashscreenView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [],
+      ),
+    );
+  }
+}
